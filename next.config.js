431e649accd48e271c49c1a1607688f7db43f216
Almar <almar.assetbekov@joyn.de>
2024-01-04 01:13:33 +0100

    chore: Update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: 'https://assetbekov-almar.github.io/go-todo',
 };
 
 module.exports = nextConfig;
