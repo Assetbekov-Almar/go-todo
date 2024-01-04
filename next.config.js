06fa854d57981a8c3642c98b81923a60ce62887b
Almar <almar.assetbekov@joyn.de>
2024-01-04 01:01:53 +0100

    chore: Update config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'todo-go.fly.dev/',
-                port: '8080',
-                pathname: '/static/**',
-            },
-        ],
-    },
 };
 
 module.exports = nextConfig;
