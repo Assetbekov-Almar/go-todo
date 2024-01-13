d6736eea7d9bf41002926b6779889f4b9dccac6e
Almar <almar.assetbekov@joyn.de>
2024-01-13 03:33:27 +0100

    chore: Adjust refresh handler to retreive lastLogout field

--- backend/authhandlers/refresh_hanlder.go
+++ backend/authhandlers/refresh_hanlder.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"package/utils"
+	"time"
 
 	"github.com/golang-jwt/jwt"
 )
@@ -37,7 +38,34 @@ func (h *AuthHandler) RefreshHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	accessToken, refreshToken, errMsg := generateJWT()
+	var userID string
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		if id, ok := claims["userID"].(string); ok {
+			userID = id
+		} else {
+			utils.RespondWithError(w, http.StatusInternalServerError, "the id claim is not present")
+			return
+		}
+	} else {
+		utils.RespondWithError(w, http.StatusInternalServerError, "the token's claims cannot be cast to jwt.MapClaims")
+		return
+	}
+
+	if userID == "" {
+		utils.RespondWithError(w, http.StatusInternalServerError, "user ID is empty")
+		return
+	}
+
+	row := h.DB.QueryRow("SELECT lastlogout FROM users WHERE id = $1", userID)
+
+	var lastLogout time.Time
+    err = row.Scan(&lastLogout)
+    if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+        return
+    }
+
+	accessToken, refreshToken, errMsg := generateJWT(userID, lastLogout)
 
 	if errMsg != "" {
 		utils.RespondWithError(w, http.StatusInternalServerError, errMsg)
