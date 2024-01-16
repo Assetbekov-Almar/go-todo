a42afeec178550c2a3f8944fb1549a8ef26b24d4
Almar <almar.assetbekov@joyn.de>
2024-01-16 23:18:28 +0100

    feat: Add register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import qs from 'qs';
+import { toast } from 'react-toastify';
+
+const Login = () => {
+    const router = useRouter();
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const username = formData.get('username');
+        const password = formData.get('password');
+        axios
+            .post(
+                'http://localhost:8082/register',
+                qs.stringify({
+                    username,
+                    password,
+                }),
+                { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+            )
+            .then(() => {
+                router.push('/login');
+            })
+            .catch((error) => {
+                toast.error(error.response.data.error);
+                return;
+            });
+    };
+
+    return (
+        <div className="flex max-h-screen flex-col items-center justify-between p-24">
+            <form className="flex flex-col items center gap-6" onSubmit={onSubmit}>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="username">Username</label>
+                    <input type="username" name="username" />
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" />
+                </div>
+                <button type="submit" className="bg-lime-600 p-2 rounded">
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
